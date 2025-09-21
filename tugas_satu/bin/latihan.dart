1123d7331b7ae24b9c61a535d446fc4a6722afe1
Your Name <yuli.asta07@gmail.com>
2025-09-21 21:54:05 +0900

    yuliasta

---
+++ tugas_satu/bin/latihan.dart
@@ -0,0 +1,11 @@
+void main(){
+    String nama = "yuli";
+    int umur = 20;
+    double tinggi_badan = 64;
+    dynamic status = "aktif";
+
+    print(nama);
+    print(umur);
+    print(tinggi_badan);
+    print(status);
+}
