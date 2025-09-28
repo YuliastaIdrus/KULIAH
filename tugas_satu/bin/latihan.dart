cefb1de93a3d985b85c885f7a61ceb7c45f2f3f7
Your Name <yuli.asta07@gmail.com>
2025-09-28 21:28:04 +0900

    Program Sistem Penilaian Mahasiswa dengan Conditional Statement

--- tugas_satu/bin/latihan.dart
+++ tugas_satu/bin/latihan.dart
@@ -1,11 +1,62 @@
-void main(){
-    String nama = "yuli";
-    int umur = 20;
-    double tinggi_badan = 64;
-    dynamic status = "aktif";
+// // void main(){
+// // //   int a = 6;
+// // //   int b = 10;
 
-    print(nama);
-    print(umur);
-    print(tinggi_badan);
-    print(status);
-}
+// // //   //aritmatika
+
+// // //   print("hasil modulus: ${b % a}");
+// // //   print("hasil penjumlahan: ${b + a}");
+// // //   print("hasil perkalian: ${b * a}");
+// // //   print("hasil pengurangan: ${b - a}");
+// // //   print("hasil pembagian: ${b / a}");
+// // //   print("hasil pembagian bulat: ${b ~/ a}"); 
+// // //   //perbandingan
+
+// // //   //logika
+
+
+// // //     String username = "user";
+// // //     int password = 123;
+
+// // //     print(username == "user" && password == 123);
+
+// // //  //penugasan
+// // //  //ternary
+// // // String username = "yuli";
+// // int password = 07352311028;
+
+
+// // if (username == "yuli" && password == 07352311028){
+// //     print("login berhasil");
+// // }else{
+// //     print("pipulang da");
+// // }
+
+
+
+// //  }
+
+// import "dart:io";
+
+// void main(){
+    
+    
+//     stdout.write("masukkan nama:");
+
+//     String? usename = stdin.readLineSync();
+
+//     print("npm saya adalah : $username");
+
+//     stdout.write("masukkan passworda:");
+
+//     // int? inputU = stdin.readLineSync();
+
+//     print("nama saya adalah : $password");
+
+// if (username == "yuli" && password == 07352311028){
+//     print("login berhasil");
+//     }else{
+//      print("pipulang da");
+//     }
+
+// }
